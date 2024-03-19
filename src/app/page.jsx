ab17b8a502399827741564ac16506173860fbb7c
Jayesh Gadhok <jayeshgadhok@gmail.com>
2024-03-19 17:47:37 +0530

    socket path correct

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, {useEffect, useState} from "react";
-const {io} = require("socket.io-client");
+import {io} from "socket.io-client";
 import Square from "../components/Square";
 import Swal from "sweetalert2";
 
